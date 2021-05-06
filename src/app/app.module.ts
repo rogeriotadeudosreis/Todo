e1ad977c72f7609f7c0f1007f11e44b98bd3a9a2
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-05-06 11:30:17 -0300

    funcionalidade delete implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatBadgeModule } from "@angular/material/badge";
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { MatBadgeModule } from "@angular/material/badge";
     MatIconModule,
     HttpClientModule,
     MatBadgeModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -33,5 +33,14 @@ export class ReadAllComponent implements OnInit {
     })
   }
 
+  delete(id: any):void {
+    this.service.delete(id).subscribe((resposta) => {
+      if(resposta === null){
+        this.service.message('Task deletada com sucesso!');
+        this.list = this.list.filter(todo => todo.id !== id);
+      }
+    })
+  }
+
   
 }
