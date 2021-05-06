dfd7fa45e9a317bb477a5c776342f56d01a7b760
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-05-06 15:51:13 -0300

    apenas testando

--- src/main/java/com/rogerioreis/todo/entities/Todo.java
+++ src/main/java/com/rogerioreis/todo/entities/Todo.java
@@ -26,7 +26,7 @@ public class Todo implements Serializable {
 	private String titulo;
 	private String descricao;
 	
-	@JsonFormat
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataParaFinalizar;
 	private Boolean finalizado = false;
 
--- src/main/java/com/rogerioreis/todo/services/DBService.java
+++ src/main/java/com/rogerioreis/todo/services/DBService.java
@@ -20,20 +20,18 @@ public class DBService {
 
 
 	public void instanciaBaseDeDados() throws ParseException {
-
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 		Todo t1 = new Todo(null, "Estudar", "Estudar Spring Boot 2 e Angular 11",
-				sdf.parse("05/05/2021"), false);
-		Todo t2 = new Todo(null, "Estudar", "Estudar banco de Dados",
-				sdf.parse("05/05/2021"), true);
-		Todo t3 = new Todo(null, "Estudar", "Estudar padrão de projeto Build",
-				sdf.parse("05/05/2021"), false);
-		Todo t4 = new Todo(null, "Estudar", "Estudar auth",
-				sdf.parse("05/05/2021"), true);
+				sdf.parse("10/05/2021"), false);
+		Todo t2 = new Todo(null, "Ler", "Ler sobre banco de Dados",
+				sdf.parse("10/05/2021"), true);
+		Todo t3 = new Todo(null, "Pesquisar", "Pesquisar padrão de projeto Build",
+				sdf.parse("10/05/2021"), false);
+		Todo t4 = new Todo(null, "Implementar", "Implementar auth",
+				sdf.parse("10/05/2021"), true);
 
 		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
 
-	}
-
+	} 
 }
