269ffed92a4b2fa94446dfea87e077570e8963c3
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-05-07 15:07:34 -0300

    todo finalizado implementado

--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -23,29 +23,36 @@ export class ReadAllComponent implements OnInit {
 
   findAll(): void {
     this.service.findAll().subscribe((resposta) => {
-    resposta.forEach(todo => {
-      if(todo.finalizado){
-        this.listFinished.push(todo);
-      } else {
-        this.list.push(todo);
-      }
-    })
-    this.closed = this.listFinished.length
-    })
+      resposta.forEach((todo) => {
+        if (todo.finalizado) {
+          this.listFinished.push(todo);
+        } else {
+          this.list.push(todo);
+        }
+      });
+      this.closed = this.listFinished.length;
+    });
   }
 
-  delete(id: any):void {
+  finalizar(item: Todo): void {
+    item.finalizado = true;
+    this.service.update(item).subscribe(() => {
+      this.service.message("Task finalizada com sucesso!");
+      this.list = this.list.filter((todo) => todo.id !== item.id);
+      this.closed++;
+    });
+  }
+
+  delete(id: any): void {
     this.service.delete(id).subscribe((resposta) => {
-      if(resposta === null){
-        this.service.message('Task deletada com sucesso!');
-        this.list = this.list.filter(todo => todo.id !== id);
+      if (resposta === null) {
+        this.service.message("Task deletada com sucesso!");
+        this.list = this.list.filter((todo) => todo.id !== id);
       }
-    })
+    });
   }
 
   navegarParaFinalizados(): void {
-    this.router.navigate(['finalizados'])
+    this.router.navigate(["finalizados"]);
   }
-
-  
 }
