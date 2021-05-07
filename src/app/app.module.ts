234be4a5706577f38ea38f6f04553fc4e215a40d
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-05-07 15:47:03 -0300

    componente create (parte 1) HTML e CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 
@@ -14,8 +15,12 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatBadgeModule } from "@angular/material/badge";
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { FinalizadosComponent } from './components/finalizados/finalizados.component';
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { FinalizadosComponent } from "./components/finalizados/finalizados.component";
+import { CreateComponent } from "./component/create/create.component";
+import { MatInputModule } from "@angular/material/input";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatNativeDateModule } from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -24,9 +29,10 @@ import { FinalizadosComponent } from './components/finalizados/finalizados.compo
     FooterComponent,
     ReadAllComponent,
     FinalizadosComponent,
+    CreateComponent,
   ],
   imports: [
-    BrowserModule,                                                    
+    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
@@ -36,6 +42,10 @@ import { FinalizadosComponent } from './components/finalizados/finalizados.compo
     HttpClientModule,
     MatBadgeModule,
     MatSnackBarModule,
+    FormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
