2dfdfe6dda88746e266668d5517acdf25b7c9a67
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-05-07 16:49:48 -0300

    componente create (parte 3) método criar implementado

--- src/app/component/create/create.component.ts
+++ src/app/component/create/create.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { Todo } from "src/app/models/todo";
+import { TodoService } from "src/app/services/todo.service";
 
 @Component({
   selector: "app-create",
@@ -7,11 +9,36 @@ import { Router } from "@angular/router";
   styleUrls: ["./create.component.css"],
 })
 export class CreateComponent implements OnInit {
-  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  todo: Todo = {
+    titulo: '',
+    descricao: '',
+    dataParaFinalizar: new Date(),
+    finalizado: false
+  }
+  constructor(private router: Router, private service: TodoService) {}
+
+  ngOnInit(): void {    
+  }
+
+  create(): void {
+    this.formataData()
+    this.service.create(this.todo).subscribe((resposta) => {
+      this.service.message ('To-do criado com sucesso!');
+      this.router.navigate(['']);
+    }, err => {
+      this.service.message('Falha ao criar To-do');
+      this.router.navigate(['']);
+    })
+    console.log(this.todo.dataParaFinalizar)
+  }
 
   cancel(): void {
     this.router.navigate([""]);
+  }
+
+  formataData():void {
+    let data = new Date(this.todo.dataParaFinalizar)
+    this.todo.dataParaFinalizar = `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`
   }
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -11,27 +11,31 @@ import { environment } from "src/environments/environment";
 export class TodoService {
   baseUrl = environment.baseUrl;
 
-  constructor(private http: HttpClient, private snack: MatSnackBar) { }
+  constructor(private http: HttpClient, private snack: MatSnackBar) {}
 
   findAll(): Observable<Todo[]> {
     return this.http.get<Todo[]>(this.baseUrl);
   }
 
   update(todo: Todo): Observable<Todo> {
-    const url = `${this.baseUrl}/${todo.id}`
+    const url = `${this.baseUrl}/${todo.id}`;
     return this.http.put<Todo>(url, todo);
   }
 
-  delete(id: any):Observable<void>{
-    const url = `${this.baseUrl}/${id}`
+  delete(id: any): Observable<void> {
+    const url = `${this.baseUrl}/${id}`;
     return this.http.delete<void>(url);
   }
 
-  message(msg: String): void{
-    this.snack.open(`${msg}`, 'OK', {
-      horizontalPosition: 'end',
-      verticalPosition: 'top' ,
-      duration: 4000
-    })
+  create(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>(this.baseUrl, todo);
+  }
+
+  message(msg: String): void {
+    this.snack.open(`${msg}`, "OK", {
+      horizontalPosition: "end",
+      verticalPosition: "top",
+      duration: 4000,
+    });
   }
 }
