d3e23cb0615ae507a9ce56e3d72cb0678a627d8a
Rogerio Reis <rogerio.rtr46@gmail.com>
2021-09-20 10:56:03 -0300

    adicionados componentes

--- src/app/components/update/update.component.ts
+++ src/app/components/update/update.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Todo } from "src/app/models/todo";
 import { TodoService } from "src/app/services/todo.service";
 
@@ -15,9 +15,28 @@ export class UpdateComponent implements OnInit {
     dataParaFinalizar: new Date(),
     finalizado: false,
   };
-  constructor(private router: Router, private service: TodoService) {}
+  constructor(private router: Router, private service: TodoService, private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.todo.id = this.route.snapshot.paramMap.get("id")!;
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.todo.id).subscribe ((resposta) => {
+      this.todo = resposta;
+    }) 
+  }
+
+  update(): void {
+    this.service.update(this.todo).subscribe ((resposta) => {
+      this.service.message('Informações atualizadas com sucesso!');
+      this.router.navigate(['']);
+    }, error => {
+      this.service.message('Falha ao atualizar To-do') ;
+      this.router.navigate(['']);
+    })
+  }
 
   cancel(): void {
     this.router.navigate([""]);
